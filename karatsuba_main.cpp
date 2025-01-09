d98b5b4a469ccd493443f8fdf44bfaa6ebcc2e56
TatianaNegodaeva <113427917+TatianaNegodaeva@users.noreply.github.com>
2025-01-09 19:03:23 +0300

    Add files via upload

--- karatsuba_main.cpp
+++ karatsuba_main.cpp
@@ -4,18 +4,14 @@
 using namespace std;
 
 int main() {
-    string x, y;
+    int x, y;
 
     cout << "Введите первое число: ";
     cin >> x;
     cout << "Введите второе число: ";
     cin >> y;
 
-    // Преобразуем строки в целые числа
-    int intX = stoi(x);
-    int intY = stoi(y);
-
-    int result = karatsuba(intX, intY);
+    int result = karatsuba(x, y);
     cout << "Результат: " << result << endl;
 
     return 0;
